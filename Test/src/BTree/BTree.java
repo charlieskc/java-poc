519cb37b803cb43a1edf3a3ed4db9898577df490
Siu Kai Ching, Charlie <charlieskc@gmail.com>
2017-02-11 19:32:13 +0800

    BTree traversal example

--- Test/src/BTree/BTree.java
+++ Test/src/BTree/BTree.java
@@ -5,12 +5,18 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+/*
+ * A binary tree implementation
+ * value = itself node value
+ * left = child left node
+ * right = child righ node
+ */
 public class BTree {
 
 	public String value;
 	public BTree left;
 	public BTree right;
-	
+
 	public BTree(String s){value=s;}
 
 	public String getValue() {
@@ -36,7 +42,7 @@ public class BTree {
 	public void setRight(BTree right) {
 		this.right = right;
 	}
-	
+
 	public void depthFirst(BTree node){
 		System.out.println(node.getValue());
 		if(node.getLeft()==null && node.getRight()==null)
@@ -46,27 +52,24 @@ public class BTree {
 		if(node.getRight() != null)
 			depthFirst(node.getRight());
 	}
-	
+
 	public void breadthFirst(BTree node, int depthOfTree){
 		List<BTree> list = new LinkedList<BTree>();
 		list.add(node);
 		Map<Integer, List<BTree>>  m = new HashMap<Integer, List<BTree>>();
 		int depth = 0;
 		m.put(Integer.valueOf(depth), list);
-		
+
 		while(true){
 			List<BTree> childList = new LinkedList<BTree>();
-			//if(list.size()==0)
-			//	break;
 			for(int i=0; i<list.size();i++){
 				BTree current = list.get(i);
-				System.out.println("transversing " + current.getValue());
+				//System.out.println("transversing " + current.getValue());
 				if (current.getLeft()!=null)
 					childList.add(current.getLeft());
 				if (current.getRight()!=null)
 					childList.add(current.getRight());
 			}
-			//depth++;
 			if(childList.size()!=0)
 				m.put(Integer.valueOf(++depth), childList);
 			else
@@ -75,7 +78,7 @@ public class BTree {
 		}
 
 		int i=0;
-		
+
 		if(depthOfTree == -1)
 			while(true){
 				if(!m.containsKey(Integer.valueOf(i)))
@@ -93,22 +96,21 @@ public class BTree {
 				System.out.print("No element at such depth - " + depthOfTree);
 				return;
 			}
-				List<BTree> depthList = m.get(Integer.valueOf(depthOfTree));
+			List<BTree> depthList = m.get(Integer.valueOf(depthOfTree));
 			System.out.print("At depth " + depthOfTree + ": ");
 			for ( BTree n:depthList){
 				System.out.print(n.getValue() + " ");
 			}
 			System.out.println();
 		}
-			
+
 	}
-	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		BTree root = new BTree("1");
-		
 		BTree node =  new BTree("2");
 		root.setLeft(node);
 		node =  new BTree("3");
@@ -121,14 +123,21 @@ public class BTree {
 		root.left.right.setLeft(node);
 		node =  new BTree("5.2");
 		root.left.right.setRight(node);
+		node =  new BTree("5.3");
 		root.left.right.left.setRight(node);
 		node =  new BTree("6");
 		root.right.setLeft(node);
 		node =  new BTree("7");
 		root.right.setRight(node);
-		
+
 		//root.depthFirst(root);
-		root.breadthFirst(root, 5);
+		//show nodes for each depth
+		System.out.println("List all depth");
+		root.breadthFirst(root, -1);
+		//show particular depth
+		System.out.println("Searching depth =3");
+		root.breadthFirst(root, 3);
+
 	}
 
 }
